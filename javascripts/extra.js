86f8606f80bb0e550f546c8c71ebd87ecfd78846
Akira Sasaki <gclue.akira@gmail.com>
2018-12-16 17:03:29 +0900

    Deployed 27111ef with MkDocs version: 1.0.4

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -4,10 +4,11 @@ window.onload = function() {
         if(tag[i].alt == "YOUTUBE") {
         	var src = tag[i].src;
         	src = src.match(".+/(.+?)$")[1];
-        	tag[i].outerHTML = "<video width=\"320\" height=\"240\" controls>\
-        	<source src=\"https://www.youtube.com/embed/c7QXYE1aX-4\">\
-        	Your browser does not support the video tag.\
-        	</video>";
+        	tag[i].outerHTML = "<iframe width=\"560\" height=\"315\"\
+        	src=\"https://www.youtube.com/embed/" + src + "\"\
+        	frameborder=\"0\"\
+        	allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"\ 
+        	allowfullscreen></iframe>";
         }
     }
 }
