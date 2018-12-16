dad39af17c43287f0be20a41d7cf294237858e7c
Akira Sasaki <gclue.akira@gmail.com>
2018-12-16 16:30:07 +0900

    Deployed 27111ef with MkDocs version: 1.0.4

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -3,8 +3,9 @@ window.onload = function() {
     for (var i = 0; i < tag.length; i++) {
         if(tag[i].alt == "YOUTUBE") {
         	var src = tag[i].src;
-        	srcn = src.match(".+/(.+?)$")[1]
+        	src = src.match(".+/(.+?)$")[1]
         	alert(src);
+        	tag[i].innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
         }
     }
 }
