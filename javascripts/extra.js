aaf8d371267f0df379ae34ecd0faff1ca424c48a
Akira Sasaki <gclue.akira@gmail.com>
2018-12-16 16:28:21 +0900

    Deployed 27111ef with MkDocs version: 1.0.4

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     var tag = document.getElementsByTagName('img');
     for (var i = 0; i < tag.length; i++) {
-        if(imgs[i].alt == "YOUTUBE") {
-        	var src = imgs[i].src;
+        if(tag[i].alt == "YOUTUBE") {
+        	var src = tag[i].src;
         	srcn = src.match(".+/(.+?)$")[1]
         	alert(src);
         }
