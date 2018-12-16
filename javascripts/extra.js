ca520588017895e8e95d6a7b1ad88d60e8b53076
Akira Sasaki <gclue.akira@gmail.com>
2018-12-16 16:26:40 +0900

    Deployed 27111ef with MkDocs version: 1.0.4

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -2,7 +2,9 @@ window.onload = function() {
     var tag = document.getElementsByTagName('img');
     for (var i = 0; i < tag.length; i++) {
         if(imgs[i].alt == "YOUTUBE") {
-        	alert("YOUTUBE");
+        	var src = imgs[i].src;
+        	srcn = src.match(".+/(.+?)$")[1]
+        	alert(src);
         }
     }
 }
