200ec2dd46ad965c3784ace5c475c16dfbeebdde
Akira Sasaki <gclue.akira@gmail.com>
2018-12-16 16:39:35 +0900

    Deployed 27111ef with MkDocs version: 1.0.4

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -3,8 +3,7 @@ window.onload = function() {
     for (var i = 0; i < tag.length; i++) {
         if(tag[i].alt == "YOUTUBE") {
         	var src = tag[i].src;
-        	src = src.match(".+/(.+?)$")[1]
-        	alert(src);
+        	src = src.match(".+/(.+?)$")[1];
         	tag[i].innerHTML = "<video width=\"320\" height=\"240\" controls>\
         	<source src=\"https://www.youtube.com/embed/c7QXYE1aX-4\">\
         	Your browser does not support the video tag.\
