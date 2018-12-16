cc13fadf9fa8b2ccde24abf9c2980388efbcbb94
Akira Sasaki <gclue.akira@gmail.com>
2018-12-16 17:08:59 +0900

    Deployed 27111ef with MkDocs version: 1.0.4

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -7,9 +7,7 @@ window.onload = function() {
         	tag[i].outerHTML 
         	var out = "<iframe width=\"560\" height=\"315\"";
         	out += "src=\"https://www.youtube.com/embed/" + src + "\"";
-        	out += "frameborder=\"0\"\
-        	allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"\ 
-        	allowfullscreen></iframe>";
+        	out += "frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
         	tag[i].outerHTML = out;
         }
     }
